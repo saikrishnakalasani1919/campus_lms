cd60a7766e5e90d4fd4a9a573e4e651da2ce70d1
saikrishna <saikrishnakalasani@gmail.com>
2025-04-14 12:58:21 +0530

    updated

--- static/ai.js
+++ static/ai.js
@@ -9,8 +9,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyACnrgy2RYbyJ7M4lkBfHc3RhSWxi6cq2c";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+
 
 const userData = {
   message: null,
